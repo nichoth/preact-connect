42e0a1a55f9478716fd4bc811cb87de09f4e41af
Nick Thomas <nichoth@gmail.com>
2017-10-28 17:58:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# preact connect
+
+Observe view events and subscribe to state changes
+
+## example
+
+```js
+var { h, render } = require('preact')
+var connect = require('../')
+var Bus = require('events').EventEmitter
+var bus = new Bus()
+var state = State({ hello: 'world' })
+
+var view = connect.subscribe(state, connect.observe(bus, MyView))
+
+bus.on('hello', console.log.bind(console, 'hello event'))
+bus.on('hello', function (ev) {
+    ev.preventDefault()
+    state.set({ hello: ev.target.elements.hello.value })
+})
+
+render(h(view, {}, []), document.body)
+
+
+// ------------------------------
+
+function MyView (props) {
+    return h('form', { onSubmit: props.emit('hello') }, [
+        h('h1', {}, ['hello ' + props.hello]),
+        h('input', { type: 'text', name: 'hello' }, []),
+        h('br', {}, []),
+        h('button', { type: 'submit' }, ['hello'])
+    ])
+}
+
+function State (data) {
+    var listener
+    function state (_listener) {
+        if (!_listener) return data
+        listener = _listener
+    }
+    state.set = function (_data) {
+        data = _data
+        if (listener) listener(data)
+    }
+    return state
+}
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+var { h, Component } = require('preact')
+var xtend = require('xtend')
+
+function subscribe (state, View) {
+    if (!View) return function (_view) {  // curry
+        return subscribe(state, _view)
+    }
+
+    return class extends Component {
+        constructor (props) {
+            super(props)
+            this.state = state()
+        }
+
+        componentDidMount () {
+            var self = this
+            this.stopListening = state(function onChange (data) {
+                self.setState(data)
+            })
+        }
+
+        componentWillUnmount () {
+            this.stopListening()
+        }
+
+        render (props, state) {
+            return h(View, xtend(props, state), props.children)
+        }
+    }
+}
+
+function observe (bus, View) {
+    if (!View) return function (_view) {  // curry
+        return observe(bus, _view)
+    }
+    return function Observed (props) {
+        return h(View, xtend(props, {
+            emit: function _emit (eventName, data) {
+                if (data === undefined) return function (_data) {
+                    return _emit(eventName, _data)
+                }
+                return bus.emit(eventName, data)
+            }
+        }), props.children)
+    }
+}
+
+function connect (state, bus, view) {
+    return subscribe(state, observe(bus, view))
+}
+connect.subscribe = subscribe
+connect.observe = observe
+
+module.exports = connect
+
